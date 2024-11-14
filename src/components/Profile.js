fc8b915137d25b014f27dcef0009f11f7925edec
AnusitCS65 <654234040@parichat.skru.ac.th>
2024-11-14 14:20:59 +0700

    profile

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -6,16 +6,10 @@ function Profile() {
     <div className="profile-content">
       <h1>Anusit Panpimsen</h1>
       <h2>Full-Stack Developer</h2>
-      <p>Hello, my name is Bank. I have developed skills in 
-        HTML, CSS, JavaScript, 
-        and Node.js, which I use to build 
-        and develop web applications. 
-        I am passionate about pursuing 
-        a career in development and 
-        am committed to continuously 
-        learning new technologies to 
-        enhance my skills and produce 
-        high-quality work in the future.</p>
+      <p>Hello, my name is Bank. I have a strong interest
+        in software development, with a foundational
+        knowledge in web programming and the ability
+          to connect to databases.</p>
       <p>Name: Anusit Panpimsen</p>
       <p>Email: anusit.panpimsen@gmail.com</p>
       <p>Phone: 082-0021492</p>
