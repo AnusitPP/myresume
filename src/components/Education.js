536d6ac42187932f0e29c24668f3ad0c9269b1d2
AnusitCS65 <654234040@parichat.skru.ac.th>
2024-11-20 14:57:00 +0700

    gpa and transcript

--- src/components/Education.js
+++ src/components/Education.js
@@ -5,6 +5,7 @@ function Education() {
     <div className="Education-content">
         <h1>Education</h1>
         <h2>Bachelor's degree in Computer Science - Faculty of Science and Technology (2022 -Present)</h2>
+        <h2>GPA : 3.56</h2>
         <p>Studying various subjects including:</p>
         <p>- Data Structure</p>
         <p>- Database and SQL</p>
@@ -13,6 +14,7 @@ function Education() {
         <p>- System Analysis and Design</p>
         <p>- Software Engineering</p>
         <p>- Data Science</p>
+        <a href='https://github.com/AnusitPP/Transcript/blob/main/Transcript.pdf'><p>Transcript</p></a>
     </div>
   );
 }
